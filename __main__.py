e7f297d39a3c0cd438ad060f25392ba783fadaf3
Abinesh Sarvepalli <abipalli@Abis-MacBook-Pro.local>
2018-05-17 19:11:35 -0700

    update workflow (file header for new workflow configuration)

--- __main__.py
+++ __main__.py
@@ -2,25 +2,25 @@
 This workflow was intended to integrate OpenMS modules with GNPS.
 
 The workflow operates as follows:
-1. FeatureFinderMetabo
-2. IDMapper
-3. MapAlignerPoseClustering
-4. MetaboliteAdductDecharge
-5. FeatureLinkerUnlabeledQT
-6. GNPSExport
+1. mzml         -> FeatureFinderMetabo          -> featureXml
+2.              -> IDMapper
+3. featureXml   -> FeautureLinkerUnlabeledQT    -> consensusXml
+4. consensusXml -> FileConverter                -> featureXml
+5. featureXml   -> MetaboliteAdductDecharger    -> featureXml & consensusXml
+6. consensusXml -> GNPSExport                   -> mgf
 '''
 
 import os
 import sys
 
 # tmp_files = {0:'mapaligner', 1:'featurefindermetabo', 2:'metaboliteadductdecharge', 3:'in_3b.featureXML', 4:'in_4.featureXML', 5:'in_3a.featureXML', 6:'out.consensusXML'}
-outputs = []
-ini_files = {'featurefinder': "/Users/abipalli/Developer/openms_workflow/ini_steps/1_FeatureFinderMetabo",
-    'mapaligner': "/Users/abipalli/Developer/openms_workflow/ini_steps/2_MapAlignerPoseClustering",
-    'adductdecharger': "/Users/abipalli/Developer/openms_workflow/ini_steps/3_MetaboliteAdductDecharger",
-    'featurelinker': "/Users/abipalli/Developer/openms_workflow/ini_steps/4_FeatureLinkerUnlabeledQT",
-    'idmapper_ini': "/Users/abipalli/Developer/openms_workflow/ini_steps/1b_IDMapper",
-    'idmapper_id': "/Users/abipalli/Developer/openms_workflow/empty.idXML"}
+# outputs = []
+ini_files = {'featurefinder': "/Users/abipalli/Developer/openms+gnps_workflow/ini_steps/1_FeatureFinderMetabo",
+    'mapaligner': "/Users/abipalli/Developer/openms+gnps_workflow/ini_steps/2_MapAlignerPoseClustering",
+    'adductdecharger': "/Users/abipalli/Developer/openms+gnps_workflow/ini_steps/3_MetaboliteAdductDecharger",
+    'featurelinker': "/Users/abipalli/Developer/openms+gnps_workflow/ini_steps/4_FeatureLinkerUnlabeledQT",
+    'idmapper_ini': "/Users/abipalli/Developer/openms+gnps_workflow/ini_steps/1b_IDMapper",
+    'idmapper_id': "/Users/abipalli/Developer/openms+gnps_workflow/empty.idXML"}
 
 
 def usage():
@@ -55,38 +55,23 @@ def per_file_workflow_post(file, count):
     os.system(command)
     # outputs[file] = [output_3_fm,output_3_cm]
     # outputs[file] = [output_3_cm]
-    outputs.append(output_3_cm)
+    # outputs.append(output_3_cm)
 
 
 def main():
-    # 1 FeatureFinderMetabo and 2 IDMapper
+    # 1 FeatureFinderMetabo & 2 IDMapper
     for i in range(1, len(sys.argv)):
         per_file_workflow_pre(sys.argv[i], i)
 
-    # 3 MapAlignerSpectrum (using featureXML)
-    print('\n==MapAlignerPoseClustering==')
-    command = 'MapAlignerPoseClustering -ini ' + ini_files['mapaligner'] + ' -in'
-    #   module inputs
-    for i in range(1, len(sys.argv)):
-        command = command + ' ' + 'idmapper'+str(i)+'.featureXML'
-    #   module outputs
-    command = command + ' -out'
-    for i in range(1, len(sys.argv)):
-        command = command + ' mapalignerposeclustering' + str(i) + '.featureXML'
-    #   module command
-    print("COMMAND: " + command + '\n')
-    os.system(command)
 
-    # 4 MetaboliteAdductDecharger
-    for i in range(1, len(sys.argv)):
-        per_file_workflow_post(sys.argv[i], i)
-
-    # 5 FeatureLinkerUnlabeledQT
-    output_4 = ""
+    # 3 FeatureLinkerUnlabeledQT (using featureXML)
+    output_3 = ""
+    # setup branching arrays for files
+    file_1s = ["idmapper1.featureXML"]; file_2s = []
+    for i in range(2, len(sys.argv)):
+        file_2s.append("idmapper" + str(i) + ".featureXML")
+    # run FeatureLinkerUnlabeledQT
     if len(sys.argv) - 1 > 1:
-        file_1s = [outputs[0]]
-        file_2s = outputs[1:]
-
         for i in range(0, len(file_2s)):
             file_count = i
             print('\n==FeatureLinkerUnlabeledQT==')
@@ -102,16 +87,40 @@ def main():
             print("COMMAND: " + command + '\n')
             os.system(command)
 
-        output_4 = file_1s[len(file_1s)-1]
+        output_3 = file_1s[len(file_1s)-1]
     else:
-        output_4 = outputs[0]
+        output_3 = file_1s[0]
+
+
+    # 4 FileConverter
+    print("\n==FileConverter==")
+    input_4 = output_3
+    output_4 = "fileconverter.featureXML"
+    command = "FileConverter -in " + input_4 + " -out " + output_4
+    print("COMMAND: " + command + "\n")
+    os.system(command)
+
+
+    # 5 MetaboliteAdductDecharger
+    input_5 = output_4
+    print('\n==MetaboliteAdductDecharge==')
+    output_5_fm = "metaboliteadductdecharger.featureXML"
+    output_5_cm = "metaboliteadductdecharger.consensusXML"
+    command = 'MetaboliteAdductDecharger -ini ' + ini_files['adductdecharger'] + ' -in ' + input_5 + ' -out_cm ' + output_5_cm + ' -out_fm ' + output_5_fm
+    print("COMMAND: " + command + '\n')
+    os.system(command)
+
 
     # 6 GNPSExport
     print('\n==GNPSExport==')
-    output_5 = "OUT.mgf"
-    input_5 = output_4
+    input_6_cm = output_5_cm
+    input_mzml = sys.argv[1:]
+    output_6 = "OUT.mgf"
 
-    command = "GNPSExport -in " + input_5 + " -out " + output_5
+    command = "GNPSExport -in_cm " + input_6_cm + " -in_mzml "
+    for mzml_file in input_mzml:
+        command += mzml_file + " "
+    command += "-out " + output_6
     print("COMMAND: " + command + '\n')
     os.system(command)
 
